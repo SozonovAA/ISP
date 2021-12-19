b2030822c6813dfed62f1ac1cc37ea8dd540522c
Andrey Sozonov <andreysoz@yandex.ru>
2021-12-19 21:45:04 +0300

    feat: add api key

--- FinanceData/urlRequests.h
+++ FinanceData/urlRequests.h
@@ -5,11 +5,14 @@
 
 #ifndef ISP_URLREQUESTS_H
 #define ISP_URLREQUESTS_H
-
+#include <string>
 
 class urlRequests {
 public:
     void request();
+
+private:
+    const std::string API_KEY = "6UQN44IKREM4QDY3";
 
 };
 
